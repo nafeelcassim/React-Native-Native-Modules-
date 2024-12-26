9393eff8ea88d18b1654169ed780da946ff25fda
nafeel_cassim <nafeel@surge.global>
2024-12-26 21:40:00 +0530

    feat: setting up native modules using turbo

---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.h
@@ -0,0 +1,16 @@
+//
+//  RCTNativeLocalStorage.h
+//  React_Native_Native_Modules
+//
+//  Created by Nafeel Cassim on 2024-12-26.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.mm
@@ -0,0 +1,12 @@
+//
+//  RCTNativeLocalStorage.m
+//  React_Native_Native_Modules
+//
+//  Created by Nafeel Cassim on 2024-12-26.
+//
+
+#import "RCTNativeLocalStorage.h"
+
+@implementation RCTNativeLocalStorage
+
+@end
